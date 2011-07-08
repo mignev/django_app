998b04c62e0b2dc8afc6a6f281ad54555926d930
Marian Ignev <m.ignev@gmail.com>
2011-07-08 10:30:27 +0300

    tornadoAppServer.py spases fix

--- tornadoAppServer.py
+++ tornadoAppServer.py
@@ -26,22 +26,25 @@ os.environ['DJANGO_SETTINGS_MODULE'] = PROJECT_NAME + '.settings'
 define('port', type=int, default=8080)
 
 class HelloHandler(tornado.web.RequestHandler):
-  def get(self):
-    self.write('Hello from tornado')
+    def get(self):
+        self.write('Hello from tornado')
 
 def main():
-  tornado.options.parse_command_line()
-  wsgi_app = tornado.wsgi.WSGIContainer(
-    django.core.handlers.wsgi.WSGIHandler())
-  tornado_app = tornado.web.Application(
-    [
-      ('/hello-tornado', HelloHandler),
-      ('.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
-      ])
-  server = tornado.httpserver.HTTPServer(tornado_app)
-  server.listen(options.port)
-  tornado.ioloop.IOLoop.instance().start()
+    tornado.options.parse_command_line()
+
+    wsgi_app = tornado.wsgi.WSGIContainer(
+        django.core.handlers.wsgi.WSGIHandler())
+
+    tornado_app = tornado.web.Application(
+        [
+            ('/hello-tornado', HelloHandler),
+            ('.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
+        ])
+
+    server = tornado.httpserver.HTTPServer(tornado_app)
+    server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
-  with daemon.DaemonContext():
-    main()
+    with daemon.DaemonContext():
+        main()
